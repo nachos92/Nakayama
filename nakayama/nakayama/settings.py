66832f0dce9221a0dee286f7a747727246c99652
Nico <piano92@hotmail.it>
2019-09-06 21:28:08 +0200

    Aggiornato file requirements e aggiunta riga (commentata) con STATIC_ROOT

--- nakayama/nakayama/settings.py
+++ nakayama/nakayama/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Per la 'produzione'
+# STATIC_ROOT = '/home/nachos92/Nakayama/nakayama/static'
